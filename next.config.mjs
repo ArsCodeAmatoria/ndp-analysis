69c5ae35d469c76d0e6dc27faa5e50cae5ac53dd
Kojin Fox <kojinfox@gmail.com>
2025-03-07 18:27:02 -0800

    Simplify Next.js configuration for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,6 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
-    domains: ['www.voideconomy.com'],
   },
   // Removing the static export to allow client-side rendering
   // output: 'export',
